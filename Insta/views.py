cb12deaca125c1735b5cb6e76635f0fdcda14fb9
Shan Lu <sl3329@cornell.edu>
2020-02-27 20:39:54 -0500

    user.is_authenticate modified

--- Insta/views.py
+++ Insta/views.py
@@ -15,8 +15,13 @@ class HelloWorld(TemplateView):
 class PostsView(ListView):
     model = Post
     template_name = 'index.html'
+    login_url = "login"
 
     def get_queryset(self):
+
+        if not self.request.user.is_authenticated:
+            return
+
         current_user = self.request.user
         following = set()
         for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
